47fa89779bf3a2bdde95cf92eef056c4de7f5e12
Thilina Dhanajaya <adamsampath2002@gmail.com>
2024-12-10 23:05:04 +0530

    basic random password gen

--- firstproject/firstproject/urls.py
+++ firstproject/firstproject/urls.py
@@ -20,5 +20,6 @@ from passgen import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home)
+    path('',views.home),
+    path('password/',views.passgen1),
 ]
--- firstproject/passgen/views.py
+++ firstproject/passgen/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, HttpResponse
-
+from django.http import JsonResponse
+import random
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Hello world</h1> <br>,<h2> My first Django project!!</h2>')
+
+def passgen1(request):
+    char = list('abcdefghijklmnopqrstuvwxyz')
+    password = ""
+    for X in range(15):
+        password += random.choice(char)
+
+    pas = {'password': password}
+    return JsonResponse(pas)
